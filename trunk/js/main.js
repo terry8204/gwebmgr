1bf062d38aeb76b5cac79856c3df2249effbe728
xiaoxu <1017859705@qq.com>
2018-10-09 18:06:40 +0800

    reportForm

--- trunk/js/main.js
+++ trunk/js/main.js
@@ -1688,9 +1688,34 @@
   var reportForm = {
     template: document.getElementById('report-template').innerHTML,
     data: function () {
-      return {}
+      return {
+        theme: "light",
+        reportNavList: [
+          {
+            title: '行驶报表',
+            name: 'reportMar',
+            icon: 'ios-photos',
+            children: [
+              { title: '超速表报', name: 'chaosuReport', icon: 'ios-subway' },
+              { title: '位置报表', name: 'weizhiReport', icon: 'md-pin' }
+            ]
+          },
+          {
+            title: '报警报表',
+            name: 'warningMar',
+            icon: 'logo-wordpress',
+            children: [
+              { title: '全部报警', name: 'allWarning', icon: 'md-warning' },
+            ]
+          },
+        ]
+      }
     },
-    methods: {}
+    methods: {
+      selectditem: function (name) {
+        console.log(name);
+      }
+    }
   }
 
   // 后台管理
