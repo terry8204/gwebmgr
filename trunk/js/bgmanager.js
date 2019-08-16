2a288a22360bc97cdf9fee548b99d6ac59aedfb3
xiaoxu <1017859705@qq.com>
2019-08-16 15:29:40 +0800

    二级管理员的权限

--- trunk/js/bgmanager.js
+++ trunk/js/bgmanager.js
@@ -101,6 +101,10 @@ var bgManager = {
     },
     mounted: function () {
         this.userType = Cookies.get('userType');
+        if (this.userType == "2") {
+            this.navList.splice(0, 1);
+            this.navList[0].children.splice(0, 1);
+        }
     }
 }
 
