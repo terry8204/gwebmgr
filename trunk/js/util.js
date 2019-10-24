751173d8ba05d67bbc64836fafcd4cf95f8ac340
xiaoxu <1017859705@qq.com>
2019-10-24 15:54:14 +0800

    audio

--- trunk/js/util.js
+++ trunk/js/util.js
@@ -1,4 +1,55 @@
 var utils = {
+  Bytes2HexStr: function (arr) {
+    var str = "";
+
+    for (var i = 0; i < arr.length; i++) {
+
+      var tmp = arr[i].toString(16);
+
+      if (tmp.length == 1) {
+
+        tmp = "0" + tmp;
+
+      }
+
+      str += tmp;
+
+    }
+
+    return str;
+
+  },
+  HexStr2Bytes: function (str) {
+
+    var pos = 0;
+
+    var len = str.length;
+
+    if (len % 2 != 0) {
+
+      return null;
+
+    }
+
+    len /= 2;
+
+    var hexA = new Array();
+
+    for (var i = 0; i < len; i++) {
+
+      var s = str.substr(pos, 2);
+
+      var v = parseInt(s, 16);
+
+      hexA.push(v);
+
+      pos += 2;
+
+    }
+
+    return hexA;
+
+  },
   changeSingleItemData: function (records, item, propertyType) {
 
     for (var i = 0; i < records.length; i++) {
