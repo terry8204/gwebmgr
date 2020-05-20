176b9f3cf0f700ab4569768c449302deb07b4d46
xiaoxu <1017859705@qq.com>
2020-05-20 09:48:49 +0800

    区域填充颜色为空

--- trunk/js/baidumap.js
+++ trunk/js/baidumap.js
@@ -329,10 +329,10 @@ BMapClass.pt.redrawAreaAndViewPortCenter = function(bdpoints) {
     this.removePolygonOverlay();
     var styleOptions = {
         strokeColor: "red", //边线颜色。  
-        fillColor: "red", //填充颜色。当参数为空时，圆形将没有填充效果。  
+        fillColor: "", //填充颜色。当参数为空时，圆形将没有填充效果。  
         strokeWeight: 3, //边线的宽度，以像素为单位。  
         strokeOpacity: 0.8, //边线透明度，取值范围0 - 1。  
-        fillOpacity: 0.6, //填充的透明度，取值范围0 - 1。  
+        // fillOpacity: 0, //填充的透明度，取值范围0 - 1。  
         strokeStyle: 'solid' //边线的样式，solid或dashed。  
     }
     this.polygon = new BMap.Polygon(bdpoints, styleOptions);
