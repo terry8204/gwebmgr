3bb749c5a9969fa877317d6c39309658c0b90506
xiaoxu <1017859705@qq.com>
2020-08-10 18:28:25 +0800

    复制设备名称

--- trunk/js/monitor.js
+++ trunk/js/monitor.js
@@ -2876,6 +2876,40 @@ var monitor = {
             var isFullscreen = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen
             isFullscreen = !!isFullscreen;
             this.isFullMap = isFullscreen;
+        },
+        copyToClipboard:function () {
+            var text = this.currentVideoDeviceInfo.deviceName;
+            if (text.indexOf('-') !== -1) {
+                let arr = text.split('-');
+                text = arr[0] + arr[1];
+            }
+            var textArea = document.createElement("textarea");
+            textArea.style.position = 'fixed';
+            textArea.style.top = '0';
+            textArea.style.left = '0';
+            textArea.style.width = '2em';
+            textArea.style.height = '2em';
+            textArea.style.padding = '0';
+            textArea.style.border = 'none';
+            textArea.style.outline = 'none';
+            textArea.style.boxShadow = 'none';
+            textArea.style.zIndex = -99;
+            textArea.style.background = 'transparent';
+        
+            textArea.value = text;
+            document.body.appendChild(textArea);
+            textArea.select();
+        
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? '成功复制到剪贴板' : '该浏览器不支持点击复制到剪贴板';
+                new Vue().$Message.success(msg);
+            } catch (err) {
+                new Vue().$Message.error('该浏览器不支持点击复制到剪贴板');
+            }
+            setTimeout(function() {
+                document.body.removeChild(textArea);
+            }, 2000)
         }
     },
     computed: {
