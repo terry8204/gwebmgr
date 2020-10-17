d4693f51e50b29e0c675449aa9930be278b9ba53
xiaoxu <1017859705@qq.com>
2020-10-17 11:48:03 +0800

    国外经纬度纠偏

--- trunk/js/transformlatlon.js
+++ trunk/js/transformlatlon.js
@@ -31,11 +31,15 @@
 	 * @returns {*[]}
 	 */
 	function gcj02tobd09 (lng, lat) {
-		var z = Math.sqrt(lng * lng + lat * lat) + 0.00002 * Math.sin(lat * x_PI);
-		var theta = Math.atan2(lat, lng) + 0.000003 * Math.cos(lng * x_PI);
-		var bd_lng = z * Math.cos(theta) + 0.0065;
-		var bd_lat = z * Math.sin(theta) + 0.006;
-		return [bd_lng, bd_lat]
+		if (out_of_china(lng, lat)) {
+			return [lng, lat]
+		}else{
+			var z = Math.sqrt(lng * lng + lat * lat) + 0.00002 * Math.sin(lat * x_PI);
+			var theta = Math.atan2(lat, lng) + 0.000003 * Math.cos(lng * x_PI);
+			var bd_lng = z * Math.cos(theta) + 0.0065;
+			var bd_lat = z * Math.sin(theta) + 0.006;
+			return [bd_lng, bd_lat]
+		}
 	}
 	/**
 	 * WGS84转GCj02
