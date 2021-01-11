6fa61e261e4567c32406b460f95ff726d534f256
xiaoxu <1017859705@qq.com>
2021-01-11 11:46:11 +0800

    谷歌地图转发

--- trunk/js/config.js
+++ trunk/js/config.js
@@ -897,8 +897,12 @@ var baiduTrafficUrlTemplate = 'http://its.map.baidu.com:8002/traffic/TrafficTile
 //var baiduTextUrlTemplate = 'http://online{s}.map.bdimg.com/tile/?qt=tile&x={x}&y={y}&z={z}&styles=sl&v=020';
 var baiduTextUrlTemplate = 'https://gps51.com/fbaidumap/tile/?qt=tile&x={x}&y={y}&z={z}&styles=sl&v=020';
 
-var googleNormalUrlTemplate = 'http://mt{s}.google.cn/vt?lyrs=m@180000000&hl=zh-CN&gl=cn&scale=2&src=app&x={x}&y={y}&z={z}&s=Gal';
-var googleSatelliteUrlTemplate = 'http://mt{s}.google.cn/vt?lyrs=y@186&hl=zh_CN&scale=2&gl=cn&x={x}&y={y}&z={z}';
+// var googleNormalUrlTemplate = 'http://mt{s}.google.cn/vt?lyrs=m@180000000&hl=zh-CN&gl=cn&scale=2&src=app&x={x}&y={y}&z={z}&s=Gal';
+// var googleSatelliteUrlTemplate = 'http://mt{s}.google.cn/vt?lyrs=y@186&hl=zh_CN&scale=2&gl=cn&x={x}&y={y}&z={z}';
+// var googleTrafficUrlTemplate = 'https://rtt2c.map.qq.com/rtt/?z={z}&x={x}&y={y}&times=1&time={time}';
+
+var googleNormalUrlTemplate = 'http://minigps.net/vt?lyrs=m@189&scale=2&gl=cn&x={x}&y={y}&z={z}';
+var googleSatelliteUrlTemplate = 'http://minigps.net/vt?lyrs=y@189&scale=2&gl=cn&x={x}&y={y}&z={z}';
 var googleTrafficUrlTemplate = 'https://rtt2c.map.qq.com/rtt/?z={z}&x={x}&y={y}&times=1&time={time}';
 
 var aliNormalUrlTemplate = 'http://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&scale=2&style=8&x={x}&y={y}&z={z}';
