c9d9127a742af91bc77353781c4e6f350f1d83ee
xiaoxu <1017859705@qq.com>
2021-02-23 18:39:04 +0800

    修改custom 文件说明

--- trunk/custom/languageex.js
+++ trunk/custom/languageex.js
@@ -1,11 +1,11 @@
 // messages.en.login.gcustomeexperience = "minigps";
-// messages.en.login.title = "11";   
-// messages.zh.login.title = "22";
+// messages.en.login.title = "please edit title in English";   
+// messages.zh.login.title = "请修改中文标题";
 
-// messages.en.login.beiAn1 = "33";   
-// messages.en.login.beiAn2 = "44";   
-// messages.zh.login.beiAn1 = "5";
-// messages.zh.login.beiAn2 = "555";
+// messages.en.login.beiAn1 = "Edit beian1 hint in English";   
+// messages.en.login.beiAn2 = "Edit beian2 hint in English";   
+// messages.zh.login.beiAn1 = "编辑中文备案1的信息";
+// messages.zh.login.beiAn2 = "编辑中文备案2的信息";
 
-// loginPageBgUrl = 'loginbg1.jpg';
-// mainLogo = 'logo1.png';
+// loginPageBgUrl = 'login_bg.jpg 请修改为登录界面背景文件名';
+// mainLogo = 'logo.png 请修改为网页logo文件名';
