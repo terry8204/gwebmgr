0fea996eaf0f0dadea2b9a5c25b36c28a8928253
xiaoxu <1017859705@qq.com>
2021-02-26 16:09:23 +0800

    third-party

---
+++ trunk/third-party/farknow/custom/languageex.js
@@ -0,0 +1,14 @@
+
+
+ messages.en.login.title = "GLOBAL TRACKING SYSTEM";   
+ messages.zh.login.title = "知远全球定位系统";
+ messages.defaultLang = "en";
+ loginPageBgUrl = 'login_bg.png';
+ mainLogo = 'mainlogo.png';
+ 
+// messages.en.login.beiAn1 = "33";   
+// messages.en.login.beiAn2 = "44";   
+// messages.zh.login.beiAn1 = "5";
+// messages.zh.login.beiAn2 = "555";
+// loginPageBgUrl = 'loginbg1.jpg';
+// mainLogo = 'logo1.png';
